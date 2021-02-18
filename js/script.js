b4a726389cd4a77ae7b19d2659295fb26a83838e
Shinnolo <simo.28.09o@gmai.com>
2021-02-18 21:58:19 +0100

    Slider js critiques
    End

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        counter: 0,
+        critiques: [
+            {
+                crit:'Best-selling author and the most influential public intellectual in the western world right now. 1',
+                newspaper: 'The New York Times 1'
+            },
+            {
+                crit:'Best-selling author and the most influential public intellectual in the western world right now. 2',
+                newspaper: 'The New York Times 2'
+            },
+            {
+                crit:'Best-selling author and the most influential public intellectual in the western world right now. 3',
+                newspaper: 'The New York Times 3'
+            }
+        ],
+    },
+   
+           
+
+            
+
+
+
+
+
+})
